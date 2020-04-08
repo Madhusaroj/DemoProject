d87121d5674e6985014e4ba36ac252673aee60fa
mpradhan9 <mpradhan9@US-5CG8411FF6>
2020-04-08 13:07:33 +0530

    Updated TC3

--- src/main/java/com/google/pages/GoogleSearchTC3.java
+++ src/main/java/com/google/pages/GoogleSearchTC3.java
@@ -17,7 +17,7 @@ public class GoogleSearchTC3 {
         WebDriver driver = new ChromeDriver(); 
 		driver.get("http://google.co.in/");
 		driver.manage().window().maximize();
-		driver.findElement(By.name("q")).sendKeys("madhu");
+		driver.findElement(By.name("q")).sendKeys("madhusmita");
 		driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
 		Thread.sleep(3000);
 		driver.close();
